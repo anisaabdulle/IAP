6ffffb536bb518ffbd87ca2eae75af5bf5975cc6
Anisa Abdullahi <abdullahianisa9@gmail.com>
2024-09-04 14:29:42 +0300

    commit again

---
+++ fnc.php
@@ -0,0 +1,23 @@
+<?php
+
+class fnc{
+ 
+    var $fname;
+    public $username;
+    var $yob;
+    protected $email_address;
+    private $password;
+
+    public function computer_user($fname){
+        return $fname;
+    }
+
+    public function user_age($fname, $yob){
+        $age = date('Y')-$yob;
+        return $fname. " is ". $age;
+}
+}
+
+$Obj = new fnc();
+
+print $Obj->user_age("Alex", 2002);
