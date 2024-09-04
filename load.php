c8e713cd01062e3b8757ff16fdefbae66b06b977
Anisa Abdullahi <abdullahianisa9@gmail.com>
2024-09-04 14:47:05 +0300

    again

--- load.php
+++ load.php
@@ -1,5 +1,13 @@
 <?php
 
-require_once "user_details.php";
+require_once "user_details..php";
 
-$Obj = new fnc();
+$Obj = new user_details.();
+
+$arr = ["black", "white", "green", "blue"];
+
+foreach($arr as $color){
+    print $color . "<br>";
+}
+
+print dirname(__FILE__);
